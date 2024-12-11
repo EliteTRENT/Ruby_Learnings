7ca6e5f65d0eff8060193b812b88ff924d339103
Aryan Negi <negiaryan2803@gmail.com>
2024-12-11 14:58:24 +0530

    [Aryan] Modify: Modified the code to achieve exception chaining properly

--- Exception/Q8.rb
+++ Exception/Q8.rb
@@ -14,22 +14,23 @@ rng = Random.new
 begin 
   print "Enter query (select, insert, delete): "
   query = gets.chomp
-  begin
-    flag = rng.rand(0..1)
-    if flag == 0
-      raise ConnectionError.new("Connection lost!")
-    end
-  rescue ConnectionError => e
-    puts "#{e.message} Retrying..."
-    retry
+  flag = rng.rand(0..1)
+  if flag == 0
+    raise ConnectionError.new("Connection lost!")
   end
   if query.downcase != "select" && query.downcase != "insert" && query.downcase != "delete"
     raise DatabaseError.new("Invalid Query: Operation Failed!")
   end
   puts "Changes made to database: #{query} query used"
+rescue ConnectionError => e
+  begin
+    puts "Caught a ConnectionError: #{e.message}"
+    raise DatabaseError.new("Query failed due to connection error")
+  rescue DatabaseError => e
+    puts "Error: #{e.message}"
+  end
 rescue DatabaseError => e
-  puts "Caught a DatabaseError: #{e.message}"
-  puts "Enter valid query"
+  puts "#{e.message}"
   retry
 ensure 
   puts "END OF SESSION"
